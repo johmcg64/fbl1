1516024564da4be81b0fde409a39ef7c67b7d189
johmcg64 <johmcg64@gmail.com>
2013-11-05 19:41:26 -0600

    Update ui.js
    e

--- phonegap-facebook-plugin-master/example/www/js/ui.js
+++ phonegap-facebook-plugin-master/example/www/js/ui.js
@@ -4,31 +4,39 @@
 yo
 */
 
+alert("in ui.js: ")
 //show a loading screen when launched, until we get the user's session back
+
+alert("in ui.js: about to call setAction")
 setAction("Loading Hackbook", true);
+alert("in ui.js: returned from setAction")
 
 //Swaps the pages out when the user taps on a choice
 function openPage(pageName, ignoreHistoryPush) {
+ alert("1")
   window.scrollTo(0,1);
-
+alert("2")
   var els = document.getElementsByClassName('page');
-  
+  alert("3")
   for (var i = 0 ; i < els.length ; ++i) {
     els[i].style.display = 'none';
   }
-  
+  alert("4")
   var page = document.getElementById('page-' + pageName);
-  
+  alert("5")
   page.style.display = "block";
-  
+  alert("6")
   title = (pageName == 'root') ? 'Hackbook' : pageName.replace(/-/g, ' ');
+  alert("7")
   document.getElementById('title').innerHTML = title;
-  
+  alert("8")
   if (ignoreHistoryPush != true) {
+   alert("9")
     window.history.pushState({page: pageName}, '', document.location.origin + document.location.pathname + "#" + pageName);
   }
-
+alert("10")
   document.getElementById('back').style.display = (pageName == 'root') ? 'none' : 'block';
+  alert("11")
 }
 
 window.onpopstate = function(e) {
@@ -41,7 +49,9 @@ window.onpopstate = function(e) {
   }
 }
 
+alert("in ui.js: about to call openPage")
 openPage('root', true);
+alert("in ui.js: returned from openPage")
 
 //Shows a modal dialog when fetcing data from Facebook
 function setAction(msg, hideBackground) {
